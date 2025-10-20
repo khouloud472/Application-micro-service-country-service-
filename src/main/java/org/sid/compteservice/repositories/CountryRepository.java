98a9af20bce9022a02d0c070d353da6a8bb2e0dd
khouloud chrif <khouloud.chrif@edu.isetcom.tn>
2025-10-20 02:23:50 +0100

    first commit

---
+++ src/main/java/org/sid/compteservice/repositories/CountryRepository.java
@@ -0,0 +1,8 @@
+package org.sid.compteservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.sid.compteservice.beans.Country;
+
+public interface CountryRepository extends JpaRepository<Country,Integer> {
+
+}
