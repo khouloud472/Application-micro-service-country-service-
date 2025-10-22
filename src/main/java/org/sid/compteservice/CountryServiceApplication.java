e1109881a38c5afff4f2300313fedd747405855c
Khouloud CHRIF <khouloud.chrif@ensi-uma.tn>
2025-10-23 00:41:21 +0100

    Update CountryServiceApplication.java

--- src/main/java/org/sid/compteservice/CountryServiceApplication.java
+++ src/main/java/org/sid/compteservice/CountryServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@AutoConfigureMockMvc(addFilters = false) 
 public class CountryServiceApplication {
 
 	public static void main(String[] args) {
