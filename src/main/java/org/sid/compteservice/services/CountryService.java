4e3f5b01430a9753bab3d8562c731b39ec8a9dd9
khouloud chrif <khouloud.chrif@edu.isetcom.tn>
2025-10-27 05:06:10 +0100

    updates

--- src/main/java/org/sid/compteservice/services/CountryService.java
+++ src/main/java/org/sid/compteservice/services/CountryService.java
@@ -10,52 +10,32 @@ import org.sid.compteservice.beans.Country;
 import org.sid.compteservice.repositories.CountryRepository;
 
 @Service
-@Component
 public class CountryService {
-	
-	@Autowired
-	CountryRepository countryRep;
-	
-	public List <Country> getAllCountries() {
-		List <Country> countries = countryRep.findAll();
-		return countries;		
-	}
-	
-	public Country getCountryById(int id) {
-		List <Country> countries = countryRep.findAll();
-		Country country = null;
-		
-		for (Country con:countries) {
-			if (con.getIdCountry()==id)
-				country = con;			
-		}
-		
-		return country;
-	}
-	
-	public Country getCountryByName (String name) {
-		List <Country> countries = countryRep.findAll();
-		Country country = null;
-		
-		for (Country con:countries) {
-			if (con.getName().equalsIgnoreCase(name))
-				country = con;			
-		}
-		
-		return country;
-	}
-	
-	public Country addCountry(Country country) {
-		return countryRep.save(country);
-	}
-	
-	public Country updateCountry(Country country) {
-		return countryRep.save(country);
-	}
-	
-	public void deleteCountry (Country country) {
-		countryRep.delete(country);
-	}
 
-	
+    @Autowired
+    private CountryRepository countryRep;
+
+    public List<Country> getAllCountries() {
+        return countryRep.findAll();
+    }
+
+    public Country getCountryById(int id) {
+        return countryRep.findById(id).orElse(null);
+    }
+
+    public Country getCountryByName(String name) {
+        return countryRep.findByNameIgnoreCase(name);
+    }
+
+    public Country addCountry(Country country) {
+        return countryRep.save(country);
+    }
+
+    public Country updateCountry(Country country) {
+        return countryRep.save(country);
+    }
+
+    public void deleteCountry(Country country) {
+        countryRep.delete(country);
+    }
 }
