07ebc7dd35d5b1905ffc85edcc52dab2a5b2a694
Khouloud <khouloud.chrif@ensi-uma.tn>
2025-10-27 06:32:09 +0100

    update

--- src/main/java/org/sid/compteservice/CountryServiceApplication.java
+++ src/main/java/org/sid/compteservice/CountryServiceApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CountryServiceApplication {
+public class ReservationavionApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ReservationavionApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(CountryServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationavionApplication.class, args);
+    }
 }
